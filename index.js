da9a313809fe4336d2ab0e52a1a094364dafae0d
Aaron jiles <sirjiles1230@gmail.com>
2023-10-05 13:30:45 -0500

    finished

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+1.  create a let string for the phrase/mix it up to try different ways of making that
+2.  create unique variables to display the combination along with the phrase */
+
+
+let stringVar = "You have recieved this messege because you have been chosen to open an important vault. Here is the secret combination:"
+
+const one = 20/4*2
+ 
+const two = 80/4*2
+
+const three = 100-80+19
+
+
+console.log(one,two,three)
+
+alert (stringVar + one, + +two, + three)
+
+
